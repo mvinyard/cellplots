d9ef554fd664851475d1754dc005cd4a0086f724
mvinyard <mvinyard.ai@gmail.com>
2023-08-25 18:07:55 +0000

    rename

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ import os
 import sys
 
 setuptools.setup(
-    name="getzlab-pyplots",
+    name="cellplots",
     version="0.0.0",
     python_requires=">3.9.0",
     author="Michael E. Vinyard",
     author_email="mvinyard@broadinstitute.org",
-    url="https://github.com/mvinyard/getzlab-pyplots",
+    url="https://github.com/mvinyard/cellplots",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    description="A possible plotting assistant and resource for members of the Getz Lab.",
+    description="Cell & Genomics Plotting",
     packages=setuptools.find_packages(),
     install_requires=[
         "matplotlib>=3.7.2",
